1a2af1c2b92619a21bf6ac694941baaf5f2003b8
anacmqui <anacmquintino@gmail.com>
2022-10-27 10:05:24 +0200

    streamlit - add date filter

--- Childmovis.py
+++ Childmovis.py
@@ -3,16 +3,37 @@ import numpy as np
 import streamlit as st
 from datetime import time
 
-movies = pd.read_csv('Downloads/out2 (5).csv')
+kids = pd.read_csv('Downloads/out2 (5).csv')
+directors = pd.read_csv('Documents/GitHub/Project3MRS/Director question.csv')
 
+date_range = st.slider('Choose the year interval:', 1910, 2022, value = (1990, 2000))
 
-st.title('Best movies for kids')
+#st.subheader('What are the best movies of your favourite director?')
+
+#options_dir = st.selectbox('Choose the director:', directors['Director name'].unique())
+                            
+#directors['Year'] = directors['Year'].apply(lambda x: int(x))
+    
+#df_dir = directors[(directors['Director name']==options_dir) & (directors['Year'] > date_range[0]) & (directors['Year'] < date_range[1])].head(5)
+
+#st.table(df_dir[['Movie title','IMDb rating', 'Year']].sort_values(by=['IMDb rating','Year'], ascending=False))
+
+st.subheader('Best movies for kids')
 
 #st.subheader('*Where are the orders going to?*')
-date_range = st.slider('Choose the year interval:', min_valuevalue = movies['year_release'])
 
-options = st.selectbox('Choose the studio:', movies['Studio'].unique())
-                 
-                  
-df_mov = movies[movies['Studio']==options & movies['Year']==date_range].head(5)#[['movie_title','tomatometer_rating']]
-st.table(df_mov[['Movie Title','Rotten Tomatoes rating', 'Year']])
+kids['Rotten Tomatoes rating'] = kids['Rotten Tomatoes rating'].apply(lambda x: int(x))
+
+def rate(x):
+    if kids['Movie Title'] == directors['Movie title']:
+        return x
+
+kids['IMDb rating'] = directors['IMDb rating'].apply(rate)
+
+
+
+
+options_kids = st.selectbox('Choose the studio:', kids['Studio'].unique())
+                            
+df_mov = kids[(kids['Studio']==options_kids) & (kids['Year'] > date_range[0]) & (kids['Year'] < date_range[1])].head(5)#[['movie_title','tomatometer_rating']]
+st.table(df_mov[['Movie Title','Rotten Tomatoes rating', 'Year', 'IMDb rating']].sort_values(by='Year', ascending=False))
