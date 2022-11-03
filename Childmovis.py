9e292649fdfdee8941767c1502bd82f071222e39
JulietteHanau <hanaujuliette.pro@gmail.com>
2022-11-03 13:41:27 +0000

    Small changes

--- Childmovis.py
+++ Childmovis.py
@@ -3,11 +3,14 @@ import numpy as np
 import streamlit as st
 from datetime import time
 
+# Import the datasets 
 kids = pd.read_csv('https://raw.githubusercontent.com/Sebastiao199/Project3MRS/main/kids_imdb_rt1.csv')
 directors = pd.read_csv('https://raw.githubusercontent.com/Sebastiao199/Project3MRS/main/directors_rt_imdb1.csv')
 
+# Created the interval year slider 
 date_range = st.slider('Choose the year interval:', 1900, 2022, value = (1990, 2000))
 
+#Question about the best movies per director 
 st.subheader('What are the best movies of your favourite director?')
 
 options_dir = st.selectbox('Choose the director:', directors['Director name'].unique())
@@ -24,6 +27,8 @@ hide_table_row_index = """
             """
 st.markdown(hide_table_row_index, unsafe_allow_html=True)
 st.table(df_dir[['Movie title','Year','IMDb rating', 'Rotten Tomatoes rating']].sort_values(by=['IMDb rating','Year', 'Rotten Tomatoes rating'], ascending=False))
+
+#Question about the best movies for kids 
 
 st.subheader('Best movies for kids')
 
